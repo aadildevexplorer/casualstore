e3352ce2a68c0d5ee73484e09a586c044f8b93f3
aadildevexplorer <aadilkhan04610461@gmail.com>
2026-09-18 15:29:11 +0530

    Replace suspended api to working api

--- src/lib/ai.js
+++ src/lib/ai.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const API = axios.create({
   // baseURL: "http://localhost:8080/api",
-  baseURL: "https://zylomart-3bzq.onrender.com/api",
+  baseURL: "https://zylomart-production.up.railway.app/api",
 });
 
 export const sendMessage = async (messages) => {
